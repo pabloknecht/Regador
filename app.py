13b18a2d9a1689c271264adfa5ff90b04dbf2dd9
Pablo Knecht <pabloknecht@gmail.com>
2021-07-06 20:28:09 +0200

    test thread

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, flash, redirect, render_template, request, session
-
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -21,17 +20,20 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-
-
-#Set DATA pin
+#Set DATA pin for DHT sensor
 pinDHT = 4
-while True:
-    #Read Temp and Hum from DHT22
-    global h,t 
-    h,t = dht.read_retry(dht.DHT22, pinDHT)
-    #Print Temperature and Humidity on Shell window
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t,h))
-    sleep(5) #Wait 5 seconds and read again
+
+def readingDHT():
+    while True:
+        #Read Temp and Hum from DHT22
+        global h,t 
+        h,t = dht.read_retry(dht.DHT22, pinDHT)
+        #Print Temperature and Humidity on Shell window
+        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t,h))
+        time.sleep(5) #Wait 5 seconds and read again
+
+threading.Thread(target=readingDHT).start()
+
 
 @app.route('/')
 def index():
