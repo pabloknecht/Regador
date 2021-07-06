f1f8a39750d224d847617e53cd31413f301e29fe
Pablo Knecht <pabloknecht@gmail.com>
2021-07-06 19:42:07 +0200

    adding files

--- app.py
+++ app.py
@@ -1,18 +1,34 @@
 from flask import Flask, flash, redirect, render_template, request, session
+
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
 import Adafruit_DHT as dht
-from time import sleep
+import time
+import threading
+import datetime
 
 # Configure application
 app = Flask(__name__)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
 
 #Set DATA pin
-DHT = 4
+pinDHT = 4
 while True:
     #Read Temp and Hum from DHT22
-    h,t = dht.read_retry(dht.DHT22, DHT)
+    global h,t 
+    h,t = dht.read_retry(dht.DHT22, pinDHT)
     #Print Temperature and Humidity on Shell window
     print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t,h))
     sleep(5) #Wait 5 seconds and read again
