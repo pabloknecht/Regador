ac04458a63d7fb56e6fae5d4511177cc31d7e886
Pablo Knecht <pabloknecht@gmail.com>
2021-08-01 22:09:03 +0200

    test history

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, redirect, render_template, request, session, jsonify
+from flask import Flask, flash, redirect, render_template, request, session, jsonify, send_file, make_response, request
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -7,6 +7,9 @@ import time
 import sqlite3
 import threading
 import datetime
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import io
 
 # Configure application
 app = Flask(__name__)
@@ -66,10 +69,12 @@ def process():
     timestamp = data[0][0]
     return jsonify({'temperature' : temperature, 'timestamp' : timestamp, 'humidity' : humidity})
 
-"""""   
+  
 @app.route("/history", methods=["POST", "GET"])
 def history():
     if request.method == "GET":
+        render_template("history.html")
+    else:
         dates, temps, hums = getHistData(8640)
         ys = temps
         fig = Figure()
@@ -86,8 +91,43 @@ def history():
         response.mimetype = 'image/png'
         return response
         #return render_template("history.html")
-"""
 
+
+@app.route('/plot/temp')
+def plot_temp():
+	times, temps, hums = getHistData(numSamples)
+	ys = temps
+	fig = Figure()
+	axis = fig.add_subplot(1, 1, 1)
+	axis.set_title("Temperature [°C]")
+	axis.set_xlabel("Samples")
+	axis.grid(True)
+	xs = range(numSamples)
+	axis.plot(xs, ys)
+	canvas = FigureCanvas(fig)
+	output = io.BytesIO()
+	canvas.print_png(output)
+	response = make_response(output.getvalue())
+	response.mimetype = 'image/png'
+	return response
+    
+@app.route('/plot/hum')
+def plot_hum():
+	times, temps, hums = getHistData(numSamples)
+	ys = hums
+	fig = Figure()
+	axis = fig.add_subplot(1, 1, 1)
+	axis.set_title("Humidity [%]")
+	axis.set_xlabel("Samples")
+	axis.grid(True)
+	xs = range(numSamples)
+	axis.plot(xs, ys)
+	canvas = FigureCanvas(fig)
+	output = io.BytesIO()
+	canvas.print_png(output)
+	response = make_response(output.getvalue())
+	response.mimetype = 'image/png'
+	return response
 
 
 if __name__ == '__main__':
