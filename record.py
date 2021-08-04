e861ffa5fc8da561262efe663daa27dde77699e7
Pablo Knecht <pabloknecht@gmail.com>
2021-08-04 21:15:14 +0200

    js graph

--- record.py
+++ record.py
@@ -7,27 +7,27 @@ def main():
     pinDHT = 4
 
     #Open DB connection
-    database = sqlite3.connect('data.db')
+    database = sqlite3.connect('data.db', 8)
+    database.execute("PRAGMA journal_mode=WAL")
 
     while True:
+        firstReading = True
+
         #Read Temp and Hum from DHT22
         h,t = dht.read_retry(dht.DHT22, pinDHT)
-
-        #Print Temperature and Humidity on Shell window
-        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t,h))
-
-        #Register value to database
-        database.execute("INSERT INTO weather (timestamp, temperature, humidity) VALUES (CURRENT_TIMESTAMP, ?, ?)", (t, h))
-        database.commit()
-
-        #Verify the row count and delete the old
-        cursor = database.cursor()
-        cursor.execute("SELECT COUNT(*) FROM weather")
-        result = cursor.fetchall()
-        if result[0] > 3153600:
-            print("1 year of data")
         
-        time.sleep(10) #Wait 10 seconds and read again
+        if not firstReading:
+            #Print Temperature and Humidity on Shell window
+            print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t,h))
+
+            #Register value to database
+            database.execute("INSERT INTO weather (timestamp, temperature, humidity) VALUES (CURRENT_TIMESTAMP, ?, ?)", (t, h))
+            database.commit()
+            database.close()
+            time.sleep(60) #Wait 60 seconds and read again
+        else:
+            firstReading = False
+
 
 if __name__ == "__main__":
     main()
